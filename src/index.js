1927fb960b8bc88db20cd20f96a0057a0089a5ce
Tatiana Kramarenko <tanya19101996@gmail.com>
2023-01-13 20:49:55 +0200

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,73 @@
 import './css/styles.css';
+import { fetchCountries } from './js/fetchCountries';
+import Notiflix from 'notiflix';
+import debounce from 'lodash.debounce';
 
 const DEBOUNCE_DELAY = 300;
+const inputEl = document.querySelector('#search-box');
+const listEl = document.querySelector('.country-list');
+
+inputEl.addEventListener('input', debounce(onInputChange, DEBOUNCE_DELAY));
+
+function onInputChange(evt) {
+  let value = evt.target.value.trim();
+
+  if (!value) {
+    listEl.innerHTML = '';
+    return;
+  }
+
+  fetchCountries(value)
+    .then(resp => {
+      if (resp.length === 1) {
+        listEl.innerHTML = countryCardMarkup(resp);
+        return;
+      }
+
+      if (resp.length >= 2 && resp.length <= 10) {
+        listEl.innerHTML = countriesListMarkup(resp);
+        return;
+      }
+
+      if (resp.length >= 10) {
+        return Notiflix.Notify.info(
+          'Too many matches found. Please enter a more specific name.'
+        );
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      Notiflix.Notify.failure('Oops, there is no country with that name');
+      listEl.innerHTML = '';
+      return;
+    });
+}
+
+function countriesListMarkup(arr) {
+  return arr
+    .map(({ name, flags }) => {
+      return `<li class="country-elem">
+      <img class="country-flag" src="${flags.svg}" alt="flag"</img>
+        <p class="country-name">${name.official}</p>
+      </li>`;
+    })
+    .join('');
+}
+
+function countryCardMarkup(arr) {
+  return arr
+    .map(({ name, flags, capital, population, languages }) => {
+      let langs = Object.values(languages).join(', ');
+
+      return `<li class="card-element">
+        <div class="contry-thumb">
+          <img class="country-flag" src="${flags.svg}" alt="flag"</img>
+          <h2 class="country-header">${name.official}</h2>
+        </div>
+        <p><span class="country-bold">Capital: </span>${capital}</p>
+        <p><span class="country-bold">Population: </span>${population}</p>
+        <p><span class="country-bold">Languages: </span>${langs}</p>
+      </li>`;
+    })
+    .join('');
+}
---
+++ src/js/fetchCountries.js
@@ -0,0 +1,16 @@
+export { fetchCountries };
+
+const BASE_URL = 'https://restcountries.com/v3.1';
+
+function fetchCountries(name) {
+    
+  return fetch(
+    `${BASE_URL}/name/${name}?fields=name,capital,population,flags,languages`
+  ).then(resp => {
+    if (!resp.ok) {
+      throw new Error(resp.statusText);
+    }
+
+    return resp.json();
+  });
+}
